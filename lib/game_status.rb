c0907773c31c97c9c20a9d7a53439b91f1781d93
Katleiah <katleiahramos@gmail.com>
2018-03-02 01:37:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,10 +26,8 @@ def won?(board)
       position_3 = board[win_index_3]
       if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
-      #elsif board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-          #return false
-      #else
-          #return false
+      else
+          return false
         end
     end
 end
